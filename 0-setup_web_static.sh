361f596a576768c5e170b003b389ddbc458dfe95
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-03-10 17:09:19 -0700

    task0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# setup webserver
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo chown -R ubuntu:ubuntu /data
+
+echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+sudo sed -i '/^server {/a\
+    location /hbnb_static/ {\
+        alias /data/web_static/current/;\
+    }\
+' /etc/nginx/sites-available/default
+
+sudo service nginx restart
